f754d3db233aaeb321084a58aab7435fd5ee4577
Samson <westcott2real@gmail.com>
2022-03-12 20:59:44 -0800

    task 6 rework for Betty coding style

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-  char c;
-  int i;
-  long int li;
-  long long int lli;
-  float f;
-  printf("Size of a char: %ld byte(s)\n",(long unsigned int)sizeof(c));
-  printf("Size of an int: %ld byte(s)\n", (long unsigned int)sizeof(i));
-  printf("Size of a long int: %ld byte(s)\n",(long unsigned int)sizeof(li));
-  printf("Size of a long long int: %ld byte(s)\n", (long unsigned int)sizeof(lli));
-  printf("Size of a float: %ld byte(s)\n", (long unsigned int)sizeof(f));
+char c;
+int i;
+long int li;
+long long int lli;
+float f;
+ 
+printf("Size of a char: %ld byte(s)\n",(unsigned long int)sizeof(c));
+printf("Size of an int: %ld byte(s)\n", (unsigned long int)sizeof(i));
+printf("Size of a long int: %ld byte(s)\n",(unsigned long int)sizeof(li));
+printf("Size of a long long int: %ld byte(s)\n", (unsigned long int)sizeof(lli));
+printf("Size of a float: %ld byte(s)\n", (unsigned long int)sizeof(f));
 
-  return(0);
+  return (0);
 }
