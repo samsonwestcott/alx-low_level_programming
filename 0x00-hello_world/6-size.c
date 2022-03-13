df751789a8dcf06b38bace823abf1cd0ddffa54d
Samson <westcott2real@gmail.com>
2022-03-12 21:15:49 -0800

    task 6 rework for Betty coding style 5

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,7 +17,7 @@ printf("Size of a char: %ld byte(s)\n", (unsigned long int)sizeof(c));
 printf("Size of an int: %ld byte(s)\n", (unsigned long int)sizeof(i));
 printf("Size of a long int: %ld byte(s)\n",
 (unsigned long int)sizeof(li));
- printf("Size of a long long int: %ld byte(s)\n",
+printf("Size of a long long int: %ld byte(s)\n",
 (unsigned long int)sizeof(lli));
 printf("Size of a float: %ld byte(s)\n", (unsigned long int)sizeof(f));
 return (0);
